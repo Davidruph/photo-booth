ad229943328058923e14b42d3b19d98f5b95b14b
Agbugba David C. <57929456+Davidruph@users.noreply.github.com>
2024-05-08 11:32:11 +0100

    photo booth init

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,24 @@ import Link from "next/link";
 export default function Home() {
   const { isSignedIn } = useUser();
   return (
-    <main className="flex flex-col justify-center items-center gap-5 p-24">
+    <main className="flex flex-col justify-center items-center gap-5 p-24 bg-black h-screen">
       {isSignedIn ? (
         <>
           <UserButton />
           <Link href={"/dashboard"}>Go to Dashboard</Link>
         </>
       ) : (
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          <SignInButton />
-        </p>
+        <>
+          <h1 className="text-teal-50 pb-4 font-bold">
+            Wellcome to Photo Booth Application
+          </h1>
+          <Link
+            href={"/sign-in"}
+            className="p-2 border rounded-md bg-slate-950 text-teal-50"
+          >
+            Get Started
+          </Link>
+        </>
       )}
     </main>
   );
